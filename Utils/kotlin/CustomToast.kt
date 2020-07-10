ec15fa991f410593b995bd44a44dba05479e73f6
Simplation <simplationwang@gmail.com>
2020-07-10 14:37:52 +0800

    Create CustomToast.kt

---
+++ Utils/kotlin/CustomToast.kt
@@ -0,0 +1,31 @@
+package com.example.utils
+
+import android.content.Context
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.sunnyit.coal.R
+
+
+class CustomToast {
+
+    private var toast: Toast
+    private var textView: TextView
+
+    constructor(context: Context?, message: String) : this(context, message, Toast.LENGTH_SHORT)
+
+    constructor(context: Context?, message: String, duration: Int) {
+        toast = Toast(context)
+        toast.duration = duration
+        val view = View.inflate(context, R.layout.toast_custom, null)
+        textView = view.findViewById(R.id.tv_prompt)
+        textView.text = message
+        toast.view = view
+        toast.setGravity(Gravity.CENTER, 0, 0)
+    }
+
+    fun show() {
+        toast.show()
+    }
+}
