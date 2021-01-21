05407ec86d4f079953933f9c772c821deaab4492
Simplation <simplationwang@gmail.com>
2021-01-21 16:39:48 +0800

    Update and rename KeyBoardUtils.java to KeyBoardUtil.java

--- Utils/java/KeyBoardUtil.java
+++ Utils/java/KeyBoardUtil.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
-//打开或关闭软键盘
-public class KeyBoardUtils {
+// 打开或关闭软键盘
+public class KeyBoardUtil {
 	/**
 	 * 打卡软键盘
 	 * 
-	 * @param mEditText输入框
-	 * @param mContext上下文
+	 * @param mEditText 输入框
+	 * @param mContext 上下文
 	 */
 	public static void openKeybord(EditText mEditText, Context mContext) {
 		InputMethodManager imm = (InputMethodManager) mContext
@@ -23,8 +23,8 @@ public class KeyBoardUtils {
 	/**
 	 * 关闭软键盘
 	 * 
-	 * @param mEditText输入框
-	 * @param mContext上下文
+	 * @param mEditText 输入框
+	 * @param mContext 上下文
 	 */
 	public static void closeKeybord(EditText mEditText, Context mContext) {
 		InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
